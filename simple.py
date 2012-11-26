b6975b1cd5740d6f8ec20d2fd2e71291374ed005
Stardrad Yin <yin8086@gmail.com>
2012-11-27 00:29:52 +0800

    dd

--- simple.py
+++ simple.py
@@ -48,7 +48,7 @@ class Post(db.Model):
     __tablename__ = "posts"
     id    = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String())
-    slug  = db.Column(db.String(), unique=True)
+    slug  = db.Column(db.String(convert_unicode=False), unique=True)
     text  = db.Column(db.String(), default="")
     draft = db.Column(db.Boolean(), index=True, default=True)
     views = db.Column(db.Integer(), default=0)
